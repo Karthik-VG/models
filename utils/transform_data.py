b33679dda8c1287bcf616d77c36303dfbfec1807
Karthik-VG <karthikvg.engineer@gmail.com>
2022-02-24 19:40:29 +0530

    added transform data script

---
+++ utils/transform_data.py
@@ -0,0 +1,9 @@
+
+class Transform_Data:
+    def __init__(self):
+        pass
+    def linreg(x):
+        #['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
+        return x['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
+
+    
