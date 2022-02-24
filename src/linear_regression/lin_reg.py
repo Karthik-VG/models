46a7a485efcc9327fa6f1c366fa8b78e027544f6
Karthik-VG <karthikvg.engineer@gmail.com>
2022-02-24 22:18:03 +0530

    linearreg.py updated

--- src/linear_regression/lin_reg.py
+++ src/linear_regression/lin_reg.py
@@ -6,8 +6,7 @@ class Linear_regression:
     def __init__(self):
         pass
     def predict(self,x):
-        #X=transform_data.Transform_Data.linreg(x)
+        X=transform_data.Transform_Data.linreg(x)
         model=pickle.load(open("model/linearreg.pkl","rb"))
-        arr=np.array(x)
-        pred=model.predict(arr.reshape(1,-1))
+        pred=model.predict(X)
         return pred
