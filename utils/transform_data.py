287c1d92c03ddc510b36200142c597c1870f0f0e
Karthik-VG <karthikvg.engineer@gmail.com>
2022-02-24 22:18:18 +0530

    transformdata.py updated

--- utils/transform_data.py
+++ utils/transform_data.py
@@ -1,9 +1,13 @@
+import numpy as np
+
 
 class Transform_Data:
     def __init__(self):
         pass
     def linreg(x):
-        #['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
-        return x['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
+
+        inp=[x['LSTAT'], x['INDUS'], x['NOX'], x['PTRATIO'], x['RM'], x['TAX'], x['DIS'], x['AGE']]
+        result=(np.array(inp)).reshape(1,-1)
+        return result
 
     
